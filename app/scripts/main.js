4b229c642dc14fd2e923073df030363dd741f747
Emily Bond <emilymbond@yahoo.com>
2014-07-18 11:13:30 -0400

    started MVC framework

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('The Iron Yard Rocks');
+//Build the Model
+var Recipe = Backbone.Model.extend ({
+  defaults: {
+    name: ''
+  },
+});
+
+//Instantiate the Model
+var recipe = new Recipe();
+
+var RecipeCollection = Backbone.Collection.extend ({
+  model: Recipe
+});
+
+//View for the collection of recipes
+var RecipeListView = Backbone.View.extend ({
+
+});
+
+//View for the recipe that you've chosen
+var RecipeView = Backbone.View.extend ({
+
+});
