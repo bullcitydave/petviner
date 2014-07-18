dc7d58c8e8b8f407cec5dbf6894c7decba0b2c62
Emily Bond <emilymbond@yahoo.com>
2014-07-18 12:26:18 -0400

    added handlebars

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,9 @@
 //Build the Model
 var Recipe = Backbone.Model.extend ({
   defaults: {
-    name: ''
+    recipeName: '',
+    smallImgUrls: '',
+    rating: ''
   },
 });
 
@@ -12,12 +14,38 @@ var RecipeCollection = Backbone.Collection.extend ({
   model: Recipe
 });
 
-//View for the collection of recipes
+//Instantiate the Collection
+var recipeCollection = new RecipeCollection();
+recipeCollection.add([
+  {recipeName: 'chicken soup'}
+])
+
+//View for our recipe collection
 var RecipeListView = Backbone.View.extend ({
 
+    initialize:function(){
+        this.render();
+    },
+
+    render: function () {
+        var source = $('#recipe-list-template').html();
+        var template = Handlebars.compile(source);
+        var rendered = template({recipeCollection: this.collection.toJSON()})
+        this.$el.html(rendered);
+        return this;
+    }
+});
+
+//Instantiate the Recipe List view
+var recipeListView = new RecipeListView ({
+  collection: recipeCollection
 });
 
 //View for the recipe that you've chosen
 var RecipeView = Backbone.View.extend ({
+});
 
+$(document).ready(function(){
+  $('.container').append(recipeListView.render().$el);
+    return false;
 });
