9be62b2afa3b5e3469726b1b37267564a5c87ea8
Dave Seidman <bullcitydave@gmail.com>
2014-07-19 19:29:00 -0400

    Working with vine API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,37 @@
 //Build the Model
-var Recipe = Backbone.Model.extend ({
+var Vine = Backbone.Model.extend ({
   defaults: {
-    recipeName: '',
-    smallImgUrls: '',
-    rating: ''
+    postID: '',
+    created: '',
+    videoURL: ''
   },
 });
 
 //Instantiate the Model
-var recipe = new Recipe();
+var vine = new Vine();
 
-var RecipeCollection = Backbone.Collection.extend ({
-  model: Recipe,
-  url: 'http://api.yummly.com/v1/api/recipes?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup&callback=myGetJSON'
+var VineCollection = Backbone.Collection.extend ({
+  model: Vine,
+
+  url: 'https://api.vineapp.com/timelines/tags/cat',
+  parse: function(response) {
+            return response.results;
+        },
+  sync: function(method, model, options) {
+            var that = this;
+                var params = _.extend({
+                    type: 'GET',
+                    dataType: 'jsonp',
+                    url: that.url,
+                    processData: false
+                }, options);
+
+            return $.ajax(params);
+        }
 
 
 
-  // url: 'http://api.yummly.com/v1/api/recipes?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup',
+  // url: 'http://api.yummly.com/v1/api/vines?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup',
   // sync: function(method, model, options) {
   //     options.timeout = 10000;
   //     options.dataType = "jsonp";
@@ -28,28 +43,29 @@ var RecipeCollection = Backbone.Collection.extend ({
 });
 
 //Instantiate the Collection
-var recipeCollection = new RecipeCollection({
-  
+var vineCollection = new VineCollection({
+
 });
 
-//View for our recipe collection
-var RecipeListView = Backbone.View.extend ({
+//View for our vine collection
+var VineListView = Backbone.View.extend ({
 
     initialize:function(){
        this.collection.fetch({
-       success: function(){
-            {dataType: "jsonp"}
-        }
+         success: function(){
+              console.log('Yay!');
+              this.render();
+          }
       })
-       console.log('Did I make it past fetch?');
-       this.render();
+
+
     },
 
     render: function () {
-        var source = $('#recipe-list-template').html();
+        var source = $('#vine-list-template').html();
         var template = Handlebars.compile(source);
         console.log('Did I make it to render?');
-      //   var recipesJSON = $.ajax('http://api.yummly.com/v1/api/recipes?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup',{
+      //   var vinesJSON = $.ajax('http://api.yummly.com/v1/api/vines?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup',{
       //       'async': false,
       //       'global': false,
       //       'dataType': "jsonp",
@@ -57,8 +73,8 @@ var RecipeListView = Backbone.View.extend ({
       //           json = data;
       //       }
       // })
-      var rendered = template({recipeCollection: this.collection.toJSON()});
-      // var rendered = template({recipeCollection: data.matches});
+      var rendered = template({vineCollection: this.collection.toJSON()});
+      // var rendered = template({vineCollection: data.matches});
       // this.$el.html(rendered);
       $('.container').html(rendered);
       return this;
@@ -67,13 +83,13 @@ var RecipeListView = Backbone.View.extend ({
 }
 });
 
-//Instantiate the Recipe List view
-var recipeListView = new RecipeListView ({
-  collection: recipeCollection
+//Instantiate the Vine List view
+var vineListView = new VineListView ({
+  collection: vineCollection
 });
 
-//View for the recipe that you've chosen
-var RecipeView = Backbone.View.extend ({
+//View for the vine that you've chosen
+var VineView = Backbone.View.extend ({
 });
 
 
@@ -81,29 +97,11 @@ var RecipeView = Backbone.View.extend ({
 // $(function () {
 // 	'use strict';
 // 	// populate our default list view
-// 	$('.container').append(recipeListView.render().$el);
+// 	$('.container').append(vineListView.render().$el);
 // });
 
 
 
-
-function myGetJSON (url) {
-    var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': "http://api.yummly.com/v1/api/recipes?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup",
-        'dataType': "jsonp"
-
-    }).done(function(data) {
-json = data
-    console.log('About to return Json data: ' + json);
-    console.log(json);
-    return json;
-  });
-    // console.log(json);
-    // return json;
-};
 
 //
 // var j = myGetJSON();
