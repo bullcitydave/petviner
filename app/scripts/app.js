2746f4b989b65a111c59fd9d0e13643dfa04841c
Dave Seidman <bullcitydave@gmail.com>
2014-07-20 17:35:45 -0400

    Home page viewed created and functional; basic app is functional

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,28 @@
+var HomeView = Backbone.View.extend ({
+	className : 'home',
+		initialize:function(){
+			console.log('Welcome to Petviner!');
+		},
+
+		render: function () {
+				var source = $('#home-template').html();
+				// var template = Handlebars.compile(source);
+				// var rendered = template();
+				this.$el.html(source);
+				console.log('Home page rendered');
+				return this;
+
+
+}
+});
+
+var homeView = new HomeView();
+
+// Launch home page
+
 $(function () {
 	'use strict';
 
-	  // $('.container').append(vineListView.render().$el);
-		// $('.container').html(vineListView.render().$el);
+		$('.container').html(homeView.render().$el);
+
 });
