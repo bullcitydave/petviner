794a65e0b25cd844b1b97c45a2e7dfe133a49cb3
Dave Seidman <bullcitydave@gmail.com>
2014-07-20 11:47:04 -0400

    Single view not rendering, id not being generated, using postId

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,17 @@
 var Vine = Backbone.Model.extend ({
     defaults: function(){
           return {
-              postID: '',
+              postId: '',
               created: '',
               videoUrl: '',
               username: '',
               permalinkUrl:''
             };
-      }
-    })
+      },
+      idAttribute: "postId"
+    });
+
+
 
 //Instantiate the Model
 var vine = new Vine();
@@ -39,22 +42,6 @@ url: 'http://www.mocky.io/v2/53cb43667313bbe4019ef820',
 
             return $.ajax(params);
         }
-
-
-
-
-
-
-
-  // url: 'http://api.yummly.com/v1/api/vines?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup',
-  // sync: function(method, model, options) {
-  //     options.timeout = 10000;
-  //     options.dataType = "jsonp";
-  //     options.jsonp = "JSONPcallback";
-  //     return Backbone.sync(method, model, options);
-
-
-
 });
 
 //Instantiate the Collection
@@ -66,33 +53,24 @@ var vineCollection = new VineCollection({
 var VineListView = Backbone.View.extend ({
   className : 'list',
     initialize:function(){
+       var self = this;
        this.collection.fetch({
          success: function(){
               console.log('Yay!');
               console.log(this.collection);
-
           }
-      })
-      // this.render();
-
+       }).done(function(){
+           self.render();
+         });
     },
 
     render: function () {
         var source = $('#vine-list-template').html();
         var template = Handlebars.compile(source);
-        console.log('Did I make it to render?');
-      //   var vinesJSON = $.ajax('http://api.yummly.com/v1/api/vines?_app_id=2aacde19&_app_key=e8bce795e7a1dc7c96574390c998df81&q=%20chicken+soup',{
-      //       'async': false,
-      //       'global': false,
-      //       'dataType': "jsonp",
-      //       complete: function(data){
-      //           json = data;
-      //       }
-      // })
-      var rendered = template({vineCollection: this.collection.toJSON()});
-      // var rendered = template({vineCollection: data.matches});
-      this.$el.html(rendered);
-      return this;
+        var rendered = template({vineCollection: this.collection.toJSON()});
+        this.$el.html(rendered);
+        console.log('Collection rendered to page')
+        return this;
 
 
 }
@@ -103,26 +81,23 @@ var vineListView = new VineListView ({
   collection: vineCollection
 });
 
-//View for the vine that you've chosen
-var VineView = Backbone.View.extend ({
+var VineSingleView = Backbone.View.extend({
+
+    initialize: function(){
+        console.log('Initializing single vine view');
+      },
+
+    render: function(id){
+        var source = $('#vine-single-template').html();
+        var template = Handlebars.compile(source);
+          var rendered = template({vine: vineCollection.get(id).toJSON()});
+        this.$el.html(rendered);
+        return this;
+        }
+
+
 });
 
-
-
-// $(function () {
-// 	'use strict';
-// 	// populate our default list view
-// 	$('.container').append(vineListView.render().$el);
-// });
-
-
-
-
-//
-// var j = myGetJSON();
-// console.log(j);
-
-
-$(document).ready(function() {
-    $('.list').append(vineListView.render().$el);
-    });
+var vineSingleView = new VineSingleView ({
+  className : 'vine-single'
+});
