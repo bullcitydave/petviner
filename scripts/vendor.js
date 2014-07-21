e74f1d9ec9822aa878df7d6039fe1386b4e419dd
Dave Seidman <bullcitydave@gmail.com>
2014-07-21 01:28:39 -0400

    Minor formatting fixes, JSON URL returned to fixed data as live API not working

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -14888,3 +14888,93 @@ var __module0__ = (function(__dependency1__, __dependency2__, __dependency3__, _
 
   return __module0__;
 })();
+
+if (!window.VINE_EMBEDS) {
+	window.VINE_EMBEDS = true;
+	window.VINE_DEBUG = [];
+
+	function hasClass(element, className) {
+		return element.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+
+	function addClass(element, className) {
+		if (!this.hasClass(element, className)) element.className += " " + className;
+	}
+
+	function removeClass(element, className) {
+		if (hasClass(element, className)) {
+	    	var regex = new RegExp('(\\s|^)' + className + '(\\s|$)');
+			element.className = element.className.replace(regex, ' ');
+		}
+	}
+
+	window.setInterval(function () {
+		var embeds = document.querySelectorAll('iframe[src*="vine.co"]');
+		for (var i = 0; i < embeds.length; i++) {
+
+			if (hasClass(embeds[i], 'loaded')) {
+
+				var topPosition = embeds[i].getBoundingClientRect().top;
+				if (topPosition > -300 && topPosition < document.documentElement.clientHeight) {
+					if (!hasClass(embeds[i], 'playing')) {
+						window.VINE_DEBUG.push(embeds[i].src + ' entered screen');
+						addClass(embeds[i], 'playing');
+						embeds[i].contentWindow.postMessage('play', '*');
+					}
+				} else {
+					if (hasClass(embeds[i], 'playing')) {
+						removeClass(embeds[i], 'playing');
+						embeds[i].contentWindow.postMessage('pause', '*');
+					}
+				}
+			}
+
+		}
+	}, 100);
+
+	var eventMethod = window.addEventListener ? 'addEventListener' : 'attachEvent';
+	var listenForEvent = window[eventMethod];
+	var eventName = eventMethod == 'attachEvent' ? 'onmessage' : 'message';
+
+	listenForEvent(eventName, function (event) {
+		var e = event.data.split('::');
+		window.VINE_DEBUG.push(event.data);
+		if (e[0] == 'height') {
+			var embeds = document.querySelectorAll('iframe[src="' + e[1] + '"]');
+			for (var i = 0; i < embeds.length; i++) {
+				newHeight = parseInt(e[2]);
+				if (embeds[i].style.removeProperty) {
+					embeds[i].style.removeProperty('height');
+				} else {
+					embeds[i].style.removeAttribute('height');
+				}
+				embeds[i].height = newHeight * 1;
+			}
+		} else if (e[0] == 'unmute') {
+			var embeds = document.querySelectorAll('iframe[src="' + e[1] + '"]');
+			var target = null;
+			for (var i = 0; i < embeds.length; i++) {
+				target = embeds[i];
+				addClass(embeds[i], 'unmuted');
+			}
+			var embeds = document.querySelectorAll('iframe.unmuted');
+			for (var i = 0; i < embeds.length; i++) {
+				if (embeds[i] != target) {
+					removeClass(embeds[i], 'unmuted');
+					embeds[i].contentWindow.postMessage('mute', '*');
+				}
+			}
+		} else if (e[0] == 'loaded') {
+			var embeds = document.querySelectorAll('iframe[src="' + e[1] + '"]');
+			for (var i = 0; i < embeds.length; i++) {
+				embeds[i].setAttribute('frameborder', 0);
+				addClass(embeds[i], 'loaded');
+        embeds[i].contentWindow.postMessage('hasEmbedScript', '*');
+			}
+		}
+	});
+}
+
+/* Lettering.JS   0.6.1 by Dave Rupert  - http://daverupert.com */
+/* Dave Seidman: easily splits a string into single-character spans for quick styling */
+(function($){function injector(t,splitter,klass,after){var a=t.text().split(splitter),inject='';if(a.length){$(a).each(function(i,item){inject+='<span class="'+klass+(i+1)+'">'+item+'</span>'+after});t.empty().append(inject)}}var methods={init:function(){return this.each(function(){injector($(this),'','char','')})},words:function(){return this.each(function(){injector($(this),' ','word',' ')})},lines:function(){return this.each(function(){var r="eefec303079ad17405c889e092e105b0";injector($(this).children("br").replaceWith(r).end(),r,'line','')})}};$.fn.lettering=function(method){if(method&&methods[method]){return methods[method].apply(this,[].slice.call(arguments,1))}else if(method==='letters'||!method){return methods.init.apply(this,[].slice.call(arguments,0))}$.error('Method '+method+' does not exist on jQuery.lettering');return this}})(jQuery);
